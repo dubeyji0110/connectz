1543faa2af2780e75e914fc0359146c0f158674d
Devansh Dubey <devanshdubey2001@gmail.com>
2021-07-13 17:03:57 +0530

    baseurl error

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,6 @@
-const baseUrl = "http://127.0.0.1:3000";
+const baseUrl =
+	process.env.NODE_ENV === "production"
+		? "https://connectz.vercel.app/"
+		: "http://127.0.0.1:3000";
 
 export default baseUrl;
