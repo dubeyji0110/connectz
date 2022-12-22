c9c282eff9b06e56afdb09f88f635b7e0efabedd
Devansh Dubey <devanshdubey2001@gmail.com>
2022-12-22 16:01:14 +0530

    fix

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV !== "production"
 		? "http://127.0.0.1:8080"
-		: "https://connectz-app.vercel.app";
+		: "https://connectz-app.herokuapp.com";
 
 module.exports = baseUrl;
